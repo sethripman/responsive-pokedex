b9512110daf2f18377cb51c371adb2ffef996827
sethripman <sethripman@gmail.com>
2019-11-04 13:12:24 -0800

    travis?!?

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,25 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import Header from '../pokedex/Header.js';
 
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const expected = `
+            <header>
+                <h1>This is the Pokedex!</h1>
+                <hr>
+            </header>
+        `;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const newHeader = new Header();
+    const newHeaderDOM = newHeader.renderHTML();
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, true);
+    assert.equal(newHeaderDOM, expected);
 });
