cc0a655026355443a5b1d89aeb2517e863a7625e
sethripman <sethripman@gmail.com>
2019-11-05 09:44:07 -0800

    ready for heroku create

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
+app.get('/hello', (request, response) => {
+    response.status(200).send('Hello');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
